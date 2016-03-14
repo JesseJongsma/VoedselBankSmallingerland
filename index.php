51be44f82c70a35c86d07b037f6d15afe9c3ce4c
Robske <robalma7@gmail.com>
2016-03-14 15:00:00 +0100

    spul

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+<head>
+
+<title>Home</title>
+
+<!-- Bootstrap & Custom CSS -->
+<link rel="stylesheet" type="text/css" href="/vbdrachten/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/vbdrachten/custom.css">
+
+<!-- Bootstrap JS -->
+<script src="/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+	  
+      <a class="navbar-brand">PHP - Programming</a>
+	</div>
+	
+	<ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+	
+  </div>
+</nav>
+
+<div class="container">
+	<div class="col-sm-12">
+	
+	<h1>Projects:</h1>
+	<h2><a href="/projects/function-uppercase-center-letter.php" alt="">Function - Uppercase Center Letter</a></h2>
+	<h2><a href="/projects/cms-function-combi.php" alt="">Function - CMS Function Combi</a></h2>
+
+	</div>
+</div>
+
+</body>
+</html>
